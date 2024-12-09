5442fcba1f3cc61ec647cf44a26b74070dd51648
Jascha <75188000+09B13Wa@users.noreply.github.com>
2024-12-09 19:31:54 +0100

    fix( Astar ): cleaned up variable names

--- Astar.cs
+++ Astar.cs
@@ -1,36 +1,37 @@
 ﻿using SuperLinq.Collections;
 using System.Diagnostics;
+
 namespace Pathfinding_Astar
 {
     class Astar
     {
-        private (int x, int y) Departure;
-        private (int x, int y) Arrival;
-        private static int _Size = MapModel._Size;
-        public char[,] Image = new char[_Size, _Size];
-        public Node[,] Grid = new Node[_Size, _Size];
-        public UpdatablePriorityQueue<Node, double> OpenSet = new UpdatablePriorityQueue<Node, double>();
-        private Node _Current;
-        private ulong calculs = 0;
-        private Stopwatch _StopWatch = null;
-        private static object Lock = new object();
-        private static Node value;
-        private static bool IsEntered = false;
+        private readonly (int x, int y) _departure;
+        private readonly (int x, int y) _arrival;
+        private const int Size = MapModel.Size;
+        public readonly char[,] Image;
+        public readonly Node[,] Grid = new Node[Size, Size];
+        public readonly UpdatablePriorityQueue<Node, double> OpenSet = new();
+        private Node _current;
+        private ulong _calculs = 0;
+        private readonly Stopwatch _stopWatch;
+        private static readonly object _lock = new();
+        private static Node _value = null!;
+        private static bool _isEntered = false;
         public bool IsWay { get; private set; }
 
         public Astar((int x, int y) departure, (int x, int y) end)
         {
-            Departure = departure;
-            Arrival = end;
-            Node startNode = new Node(Departure.x, Departure.y);
+            _departure = departure;
+            _arrival = end;
+            Node startNode = new Node(_departure.x, _departure.y);
             OpenSet.Enqueue(startNode, startNode.FCost);
-            Grid[Departure.x, Departure.y] = startNode;
+            Grid[_departure.x, _departure.y] = startNode;
             startNode.GCost = 0;
-            startNode.FCost = HCostCalculate(Departure, Arrival);
-            Image = (char[,])BidirectionnalAstar.mapModel.Clone();
-            _StopWatch = new Stopwatch();
-            _StopWatch.Start();
-            value = new Node();
+            startNode.FCost = HCostCalculate(_departure, _arrival);
+            Image = (char[,])BidirectionnalAstar.MapModel.Clone();
+            _stopWatch = new Stopwatch();
+            _stopWatch.Start();
+            _value = new Node();
             Path();
         }
 
@@ -38,48 +39,49 @@ namespace Pathfinding_Astar
         {
             while (OpenSet.Count > 0)
             {
-                _Current = OpenSet.Dequeue();
+                _current = OpenSet.Dequeue();
 
-                Image[_Current.X, _Current.Y] = 'V';
+                Image[_current.X, _current.Y] = 'V';
                 bool check;
-                
-                lock (Lock)
+
+                lock (_lock)
                 {
-                    check = BidirectionnalAstar.MergedGrids.TryGetValue((_Current.X, _Current.Y), out value);
+                    check = BidirectionnalAstar.MergedGrids.TryGetValue((_current.X, _current.Y), out _value);
                     if (check)
                     {
-                        ReconstructPath(value);
+                        ReconstructPath(_value);
                         IsWay = true;
                         break;
                     }
-                    BidirectionnalAstar.MergedGrids.Add((_Current.X, _Current.Y), _Current);
+
+                    BidirectionnalAstar.MergedGrids.Add((_current.X, _current.Y), _current);
                 }
+
                 for (int i = -1; i < 2; i++)
                 {
                     for (int j = -1; j < 2; j++)
                     {
-                        if ((i, j) != (0, 0))
-                        {
-                            int newX = _Current.X + i;
-                            int newY = _Current.Y + j;
+                        if ((i, j) == (0, 0))
+                            continue;
+                        int newX = _current.X + i;
+                        int newY = _current.Y + j;
 
-                            if (MapModel.IsTraversable(Image, newX, newY) && Image[newX, newY] != 'V')
-                            {
-                                ProcessNeighbor(newX, newY);
-                                calculs++;
-                            }
+                        if (!MapModel.IsTraversable(Image, newX, newY))
+                            continue;
 
-                        }
+                        ProcessNeighbor(newX, newY);
+                        _calculs++;
                     }
                 }
             }
 
-            _StopWatch.Stop();
+            _stopWatch.Stop();
             if (!IsWay)
                 Console.WriteLine("Chemin impossible à réaliser");
-            Console.WriteLine("Nodes calculés : " + calculs);
-            Console.WriteLine("Temps du chemin : " + _StopWatch.ElapsedMilliseconds + " ms");
+            Console.WriteLine("Nodes calculés : " + _calculs);
+            Console.WriteLine("Temps du chemin : " + _stopWatch.ElapsedMilliseconds + " ms");
         }
+
         private void ProcessNeighbor(int x, int y)
         {
             Node neighbor;
@@ -93,14 +95,13 @@ namespace Pathfinding_Astar
                 neighbor = Grid[x, y];
             }
 
-            int newGCost = _Current.GCost + ((x != _Current.X && y != _Current.Y) ? 14 : 10);
+            int newGCost = _current.GCost + ((x != _current.X && y != _current.Y) ? 14 : 10);
             if (newGCost < neighbor.GCost)
             {
                 neighbor.GCost = newGCost;
-                neighbor.Parent = _Current;
-                neighbor.FCost = neighbor.GCost + HCostCalculate((x, y), Arrival);
+                neighbor.Parent = _current;
+                neighbor.FCost = neighbor.GCost + HCostCalculate((x, y), _arrival);
                 OpenSet.Enqueue(neighbor, neighbor.FCost);
-
             }
         }
 
@@ -110,21 +111,23 @@ namespace Pathfinding_Astar
             int dy = Math.Abs(depart.y - arrival.y);
             return 10 * Math.Max(dx, dy);
         }
+
         private void ReconstructPath(Node value)
         {
-            while ((_Current.X, _Current.Y) != Departure)
+            // Console.WriteLine($"Reconstructing path with {path} and {path2}");
+            // Console.WriteLine($"from {_departure.x}, {_departure.y} to {_arrival.x}, {_arrival.y}");
+            //
+            while ((_current.X, _current.Y) != _departure)
             {
-                Image[_Current.X, _Current.Y] = 'P';
-                _Current = _Current.Parent;
+                Image[_current.X, _current.Y] = 'P';
+                _current = _current.Parent;
             }
 
-            while ((value.X, value.Y) != Arrival)
+            while ((value.X, value.Y) != _arrival)
             {
                 Image[value.X, value.Y] = 'P';
                 value = value.Parent;
             }
-
         }
     }
-
 }
