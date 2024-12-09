ec12497624379af0958d85ed4ac24133d6f0aba3
Jascha <75188000+09B13Wa@users.noreply.github.com>
2024-12-09 19:32:52 +0100

    fix( BidirectionnalAstar.cs ): cleaned up naming and also fixed bug when merging both grids

--- BidirectionnalAstar.cs
+++ BidirectionnalAstar.cs
@@ -1,24 +1,16 @@
-﻿using SuperLinq.Collections;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Pathfinding_Astar
+﻿namespace Pathfinding_Astar
 {
     class BidirectionnalAstar
     {
-        private Astar Forward;
-        private Astar Backward;
+        private Astar _forward;
+        private Astar _backward;
         public static Dictionary<(int, int), Node> MergedGrids;
         //public static (ulong, ulong) Counter;
-        public static char[,] mapModel;
+        public static char[,] MapModel;
 
         public BidirectionnalAstar(int walls)
         {
-            mapModel = MapModel.mapGeneration(walls);
+            MapModel = Pathfinding_Astar.MapModel.mapGeneration(walls);
             MergedGrids = new Dictionary<(int, int), Node>();
             //Counter = (0, 0);
             Path();
@@ -27,32 +19,46 @@ namespace Pathfinding_Astar
         {
             Task task1 = Task.Run(() => 
             {
-                Forward = new Astar(MapModel._Departure, MapModel._End);
+                _forward = new Astar(Pathfinding_Astar.MapModel._Departure, Pathfinding_Astar.MapModel._End);
                 return Task.CompletedTask;
             });
             Task task2 = Task.Run(() =>
             {
-                Backward = new Astar(MapModel._End, MapModel._Departure);
+                _backward = new Astar(Pathfinding_Astar.MapModel._End, Pathfinding_Astar.MapModel._Departure);
                 return Task.CompletedTask;
             });
             Task.WaitAll(task1, task2);
+            
             MergeTabs();
-            MapModel.GenerateBmp(mapModel);
+            Pathfinding_Astar.MapModel.GenerateBmp(MapModel);
             
         }
         private void MergeTabs()
         {
-            for (int i = 0; i < Forward.Image.GetLength(0); i++)
+            for (int i = 0; i < _forward.Image.GetLength(0); i++)
             {
-                for (int j = 0; j < Forward.Image.GetLength(1); j++)
+                for (int j = 0; j < _forward.Image.GetLength(1); j++)
                 {
-                    if (Forward.Image[i, j] == ' ')
-                    {
-                        Forward.Image[i, j] = Backward.Image[i, j];
-                    }
+                    ColorMap(i, j);
                 }
             }
-            mapModel = Forward.Image;
+        }
+
+        private void ColorMap(int i, int j)
+        {
+            if (_forward.Image[i, j] == 'P' || _backward.Image[i, j] == 'P')
+            {
+                MapModel[i, j] = 'P';
+                return;
+            }
+
+            if (_forward.Image[i, j] == 'V' || _backward.Image[i, j] == 'V')
+            {
+                MapModel[i, j] = 'V';
+                return;
+            }
+
+            MapModel[i, j] = _forward.Image[i, j];
         }
     }
 }
